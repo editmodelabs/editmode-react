2fb4fda54b3d88b252f7a757cedb0acff84ef08b
Megan Ennis <mennis0145@gmail.com>
2020-05-12 13:31:37 +0100

    Remove babel errors. Build and publish version with chunk as class component

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env']
+                        presets: ['@babel/preset-env']
                     }
                 }
             },
