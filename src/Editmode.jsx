91c176a2e67e68efa0e2785867b3de91db03a5c8
Eric Clemmons <eric@smarterspam.com>
2020-06-04 22:16:49 -0700

    Use projectId

--- src/Editmode.jsx
+++ src/Editmode.jsx
@@ -1,19 +1,27 @@
 // @ts-check
-import React from "react";
+import React, { useEffect } from "react";
 import { BranchContext } from "./BranchContext";
 
-function Editmode(props) {
-  let script = document.createElement("script");
-  script.src = "https://www.editmode.app/assets/chunks.js";
-  script.async = true;
-  document.body.append(script);
+function Editmode({ children, projectId }) {
+  if (!projectId) {
+    throw new Error("<Editmode projectId={...}> is missing a valid projectId");
+  }
+
+  useEffect(() => {
+    window["chunksProjectIdentifier"] = "prj_yvskxAScvL8x";
+
+    const script = document.createElement("script");
+    script.src = "https://www.editmode.app/assets/chunks.js";
+    script.async = true;
+    document.body.append(script);
+  }, []);
 
   let params = new URL(document.location.href).searchParams;
   let em_branch = params.get("em_branch");
 
   return (
     <BranchContext.Provider value={{ em_branch }}>
-      {props.children}
+      {children}
     </BranchContext.Provider>
   );
 }
