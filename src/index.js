1fd3776d441211aa654d922e0d2642c0dd492373
Eric Clemmons <eric@smarterspam.com>
2020-06-11 19:37:29 -0700

    Rename ChunkProperty to ChunkFieldValue

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@
 import Editmode from "./Editmode.jsx";
 import Chunk from "./Chunk.jsx";
 import ChunkCollection from "./ChunkCollection.jsx";
-import ChunkProperty from "./ChunkProperty.jsx";
+import ChunkFieldValue from "./ChunkFieldValue.jsx";
 
 export { useChunk } from "./useChunk";
-export { Editmode, Chunk, ChunkProperty, ChunkCollection };
+export { Editmode, Chunk, ChunkFieldValue, ChunkCollection };
