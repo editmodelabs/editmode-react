9147fcc97d049aa4931ac5188689e1af3740de81
Jen <jengkarlong@gmail.com>
2020-10-06 16:11:34 +0800

    resolve more comments

--- src/ChunkCollection.jsx
+++ src/ChunkCollection.jsx
@@ -17,38 +17,36 @@ export function ChunkCollection({
 
   useEffect(() => {
     // Get data from localStorage
-    let cachedChunk = getCachedData(cacheId)
-    let data = cachedChunk && JSON.parse(cachedChunk)
-    if (data) setChunk(data)
+    const cachedChunk = getCachedData(cacheId);
+    if (cachedChunk) {
+      const data = JSON.parse(cachedChunk);
+      setChunk(data);
+    }
 
     const urlParams = new URLSearchParams({
       limit,
       collection_identifier: identifier,
     });
 
-    tags.map((tag) => urlParams.append("tags[]", tag));
+    tags.forEach((tag) => urlParams.append("tags[]", tag));
 
-    let error; 
     api
       .get(`chunks?${urlParams}`)
       .then((res) => {
         storeCache(cacheId, res.data.chunks)
-        if (!data) setChunk(res.data.chunks)
+        if (!cachedChunk) setChunk(res.data.chunks)
       })
-      .catch((error) => error = error);
-    
-    if (error) {
-      console.log(
-        `Something went wrong trying to retrieve chunk collection: ${error}. Have you provided the correct Editmode identifier as a prop to your ChunkCollection component instance?`
-      );
-    }
+      .catch((error) => {
+        console.error(`Something went wrong trying to retrieve chunk collection: ${error}. Have you provided the correct Editmode identifier as a prop to your ChunkCollection component instance?`)
+        }
+      )
   }, [identifier]);
 
-  if (!chunks || !chunks.length) {
-    return <>children</>;
+  if (!chunks?.length) {
+    return <></>;
   }
   
-  const placeholderChunk = ( chunks.length && {...chunks[0], placeholder: true} ) || {}
+  const placeholderChunk = chunks.length ? {...chunks[0], placeholder: true} : {}
 
   return (
     <div className={className + " chunks-collection-wrapper"} data-chunk-cache-id={cacheId} data-chunk-collection-identifier={identifier}>
@@ -62,12 +60,13 @@ export function ChunkCollection({
         ))
       }
       {
-        chunks.length &&
+        chunks.length &&(
           <ChunkCollectionContext.Provider key={chunks[0].identifier + "dummy"} value={placeholderChunk}>
             <div className={itemClass + " chunks-col-placeholder-wrapper chunks-hide"}>
               {children}
             </div>
           </ChunkCollectionContext.Provider>
+        )
       }
       
     </div>
