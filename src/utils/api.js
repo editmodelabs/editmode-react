925a1a8cc355335c2c857d141ca1202f6afc6f4f
BosEriko <bos.eriko@gmail.com>
2020-10-26 16:04:22 +0800

    remove src/utils

--- src/utils/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-export const api = axios.create({
-  baseURL: "https://api.editmode.com/",
-  headers: {
-    Accept: "application/json",
-  },
-});
--- src/utils/index.js
+++
@@ -1,6 +0,0 @@
-export { getCachedData, storeCache } from './caching'
-export { computeContentKey } from './computeContentKey'
-export { sanitizeContent } from './sanitizeContent'
-export { api } from './api'
-
-export { renderChunk } from './renderChunk.jsx'
