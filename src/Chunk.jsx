ff420b118a9acfbd100a7e3c0e029e6c9cf5c32c
Jen <jengkarlong@gmail.com>
2020-11-16 10:58:39 +0800

    Try tag attribute

--- src/Chunk.jsx
+++ src/Chunk.jsx
@@ -1,14 +1,16 @@
 // @ts-check
 
-import React from "react";
+import React, {useEffect} from "react";
 import { useChunk } from "./useChunk";
 
 export function Chunk({ children, identifier, src, contentKey, ...props }) {
   const type = src ? "image" : undefined;
   const defaultContent = src || children;
-  const { Component } = useChunk(defaultContent, { identifier, type, contentKey });
+  const tag = props.tag || "em-span"
+  delete props.tag;
+  const { Component } = useChunk(defaultContent, { identifier, type, contentKey, tag });
 
-  return <Component {...props} />;
+  return <Component {...props} />
 }
 
 // Here for backwards-compatibility, but named exports are preferred
--- src/useChunk.js
+++ src/useChunk.js
@@ -4,9 +4,13 @@ import { useContext, useEffect, useState, useMemo } from "react";
 import { EditmodeContext } from "./EditmodeContext";
 import { api, renderChunk, computeContentKey, getCachedData, storeCache } from './utils'
 
-export function useChunk(defaultContent, { identifier, type, contentKey }) {
+export function useChunk(defaultContent, { identifier, type, contentKey, tag }) {
   const { projectId, defaultChunks } = useContext(EditmodeContext);
-  const [chunk, setChunk] = useState(undefined);
+  const [chunk, setChunk] = useState({
+    chunk_type: type || "single_line_text",
+    content: defaultContent || "",
+    content_key: contentKey
+  });
 
    if (!contentKey)  {
      contentKey = defaultContent ? computeContentKey(defaultContent) : null;
@@ -31,47 +35,34 @@ export function useChunk(defaultContent, { identifier, type, contentKey }) {
   const url = `chunks/${identifier || contentKey}?project_id=${projectId}`;
 
   useEffect(() => {
-    // Render content
     let cachedChunk = getCachedData(cacheId)
-    let newChunk = cachedChunk ? JSON.parse(cachedChunk) : fallbackChunk || {
-      chunk_type: type || "single_line_text",
-      content: defaultContent,
-      content_key: contentKey
-    }
-
+    let newChunk = cachedChunk ? JSON.parse(cachedChunk) : fallbackChunk
     if (newChunk) setChunk(newChunk)
 
     // Fetch new data
-    let error;
     api
       .get(url)
       .then((res) => {
         storeCache(cacheId, res.data)
         if (!cachedChunk) setChunk(res.data)
       }) // Store chunk to localstorage
-      .catch((error) => console.log(error)); // Set error state
-
-    if (error && identifier) {
-      console.warn(
-        `Something went wrong trying to retrieve chunk data: ${error}. Have you provided the correct Editmode identifier (${identifier}) as a prop to your Chunk component instance?`
-      );
-    }
+      .catch((error) => {
+        if (identifier) {
+          console.warn(
+            `Something went wrong trying to retrieve chunk data: ${error}. Have you provided the correct Editmode identifier (${identifier}) as a prop to your Chunk component instance?`
+          );
+        }
+      });
   }, [cacheId]);
 
-  
+
 
   if (chunk) {
     return {
-      Component(props) {
-        return renderChunk(chunk, props)
+      Component: props => {
+        return renderChunk(chunk, tag, props)
       },
       content: chunk.content
     };
-  } else {
-    return {
-      Component() {
-        return null
-      }
-    }
   }
 }
