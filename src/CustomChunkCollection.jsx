21207cd6e5ee48cffc3989cf4ad5637d19e6f987
Francis Bulus <notchera@gmail.com>
2021-05-30 19:02:34 +0100

    add input field for new collection item

--- src/CustomChunkCollection.jsx
+++ src/CustomChunkCollection.jsx
@@ -1,13 +1,22 @@
 // @ts-check
 import React from "react";
+import { ChunkCollectionContext } from "./ChunkCollectionContext";
+import { computeClassName } from "./utilities";
+
 export function CustomChunkCollection({
+  chunks,
   className,
   children,
   identifier,
   tags = [],
   limit = "",
+  itemClass = "",
 }) {
   const cacheId = identifier + limit + tags.join("");
+  const placeholderChunk = chunks.length
+    ? { ...chunks[0], placeholder: true }
+    : {};
+
   return (
     <div
       className={
@@ -19,6 +28,21 @@ export function CustomChunkCollection({
       data-chunk-collection-identifier={identifier}
     >
       {children}
+      {chunks.length && (
+        <ChunkCollectionContext.Provider
+          key={chunks[0].identifier + "dummy"}
+          value={placeholderChunk}
+        >
+          <div
+            className={computeClassName(
+              itemClass,
+              "chunks-col-placeholder-wrapper chunks-hide"
+            )}
+          >
+            {children}
+          </div>
+        </ChunkCollectionContext.Provider>
+      )}
     </div>
   );
 }
