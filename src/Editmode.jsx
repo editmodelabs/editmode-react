4c6985b83ab84c0ad24aadfb0d1b4e8ff6755345
Jen <jengkarlong@gmail.com>
2021-06-12 23:42:35 +0800

    Set editmode.js specific version

--- src/Editmode.jsx
+++ src/Editmode.jsx
@@ -13,7 +13,7 @@ export function Editmode({ children, projectId, defaultChunks }) {
     window["chunksProjectIdentifier"] = projectId;
 
     const script = document.createElement("script");
-    script.src = "https://static.editmode.com/editmode@^2.0.0/dist/editmode.js";
+    script.src = "https://static.editmode.com/editmode@2.0.0/dist/editmode.js";
     script.async = true;
     document.body.append(script);
 
