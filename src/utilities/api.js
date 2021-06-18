338560a78b7428ae06ecf8d437d844d385633ba1
Jen <jengkarlong@gmail.com>
2021-06-18 09:15:47 +0800

    replace api url

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api.editmode.com/",
+  baseURL: "https://api2.editmode.com/",
   headers: {
     Accept: "application/json",
     referrer: window.location.href
