4ecabc82ca603d1b3df25e7649759f8ec6fdc203
Jen Villaganas <jengkarlong@gmail.com>
2021-06-19 00:01:01 +0800

    Add CF worker custom API (#45)
    * replace api url
    
    * Upgrade axios transfer referrer from header to params

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -1,9 +1,11 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "https://api.editmode.com/",
+  baseURL: "https://api2.editmode.com/",
   headers: {
     Accept: "application/json",
-    referrer: window.location.href
   },
+  params: {
+    referrer: window.location.href
+  }
 });
