39b030de5e6d681bc50f2f087771ddcb0b03f7ff
Jen <jengkarlong@gmail.com>
2021-06-30 16:07:25 +0800

    Redesign watermark, accept field parameter  in useGetChunk

--- src/Watermark.jsx
+++ src/Watermark.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 const styles = {
   zIndex: 9999,
-  bottom: 2,
-  right: 3,
+  bottom: 8,
+  right: 8,
   position: "fixed",
-  opacity: 0.5,
+  opacity: 1,
   display: 'flex',
   alignItems: "center",
   background: "rgba(255,255,255, 0.9)",
   cursor: "pointer",
+  borderRadius: 5,
+  padding: "2px 5px 2px 2px",
   boxShadow: "0 1px 3px 0 rgba(0, 0, 0, 0.1),0 1px 2px 0 rgba(0, 0, 0, 0.06)"
 }
 
@@ -22,7 +24,7 @@ export default function Watermark() {
             </g>
         </g>
       </svg>
-      <span style={{fontSize: 13, fontWeight: 600}}>Made in Editmode</span>
+      <span style={{fontSize: 13, fontWeight: 600}}>Powered by Editmode</span>
     </div>
   )
 }
--- src/useGetChunk.js
+++ src/useGetChunk.js
@@ -4,10 +4,10 @@ import { storeCache, getCachedData, api } from './utilities'
 import { EditmodeContext } from "./EditmodeContext"
 import {useEffect, useState, useContext} from 'react'
 
-export const useGetChunk = (identifier) => {
+export const useGetChunk = (identifier, field = null) => {
   const [projectId, setProject] = useState(null);
   let [chunk, setChunk] = useState(undefined);
-  const cacheId = identifier + projectId;
+  const cacheId = identifier + projectId + field;
 
   useEffect(() => {
     setProject(window["chunksProjectIdentifier"])
@@ -21,8 +21,21 @@ export const useGetChunk = (identifier) => {
         storeCache(cacheId, res.data);
         if (!cachedChunk) setChunk(res.data)
       })
-      .catch((error) => console.log(error)); // Set error state
+      .catch((error) => console.error(error)); // Set error state
   }, [cacheId])
+
+  if (field && chunk && chunk.chunk_type == "collection_item") {
+    field = field.toLowerCase()
+    const fieldChunk = chunk.content.find(c =>
+      c.custom_field_identifier.toLowerCase() == field || c.custom_field_name.toLowerCase() == field
+    )
+    if (fieldChunk) {
+      setChunk(fieldChunk)
+    } else {
+      console.error(`We can't find a ${identifier} content with ${field} field`)
+      return ""
+    }
+  }
   
   return chunk && chunk.content || ""
 }
