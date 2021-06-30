99ba6eb6aa3e41fe99d263fff98a7380cbb64e71
Francis Bulus <notchera@gmail.com>
2021-06-30 17:00:36 +0100

    caching editmode provider request

--- src/Editmode.jsx
+++ src/Editmode.jsx
@@ -1,43 +1,53 @@
 // @ts-check
 import React, { useEffect, useState } from "react";
 import { EditmodeContext } from "./EditmodeContext";
+import { getCachedData, storeCache } from "./utilities";
 import { api } from "./utilities";
-import Watermark from './Watermark.jsx'
+import Watermark from "./Watermark.jsx";
 
 export function Editmode({ children, projectId, defaultChunks }) {
   const [branch, setbranch] = useState(null);
-  const [hasWaterMark, setHasWaterMark] = useState(null)
+  const [hasWaterMark, setHasWaterMark] = useState(null);
+  const [cachedData, setCachedData] = useState(null);
   if (!projectId) {
     throw new Error("<Editmode projectId={...}> is missing a valid projectId");
   }
-
+  const cacheId = projectId + "_provider";
   useEffect(() => {
+    const cachedItem = getCachedData(cacheId);
+    const data = JSON.parse(cachedItem);
     window["chunksProjectIdentifier"] = projectId;
 
     const script = document.createElement("script");
-    script.src = "https://unpkg.com/editmode-magic-editor@^0/dist/magic-editor.js";
+    script.src = "http://localhost:10001/magic-editor.js";
     document.body.append(script);
 
     let params = new URL(document.location.href).searchParams;
     setbranch(params.get("em_branch_id"));
 
-    api
-      .get(`/projects/${projectId}`)
-      .then((res) => {
-        const project = res.data
-        if (project.has_watermark) {
-          setHasWaterMark(true)
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+    if (!data) {
+      api
+        .get(`/projects/${projectId}`)
+        .then((res) => {
+          storeCache(cacheId, res.data);
+          const project = res.data;
+          if (project.has_watermark) {
+            setHasWaterMark(true);
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    } else {
+      const project = data;
+      if (project.has_watermark) setHasWaterMark(true);
+    }
   }, []);
 
   return (
     <EditmodeContext.Provider value={{ branch, projectId, defaultChunks }}>
-      { children }
-      { hasWaterMark && <Watermark /> }
+      {children}
+      {hasWaterMark && <Watermark />}
     </EditmodeContext.Provider>
   );
 }
