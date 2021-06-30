2b73f985b06556f281133b1f39880f30b072f30a
Jen <jengkarlong@gmail.com>
2021-06-30 14:25:49 +0800

    Add watermark

--- src/Editmode.jsx
+++ src/Editmode.jsx
@@ -1,10 +1,12 @@
 // @ts-check
 import React, { useEffect, useState } from "react";
 import { EditmodeContext } from "./EditmodeContext";
+import { api } from "./utilities";
+import Watermark from './Watermark.jsx'
 
 export function Editmode({ children, projectId, defaultChunks }) {
   const [branch, setbranch] = useState(null);
-
+  const [hasWaterMark, setHasWaterMark] = useState(null)
   if (!projectId) {
     throw new Error("<Editmode projectId={...}> is missing a valid projectId");
   }
@@ -18,11 +20,26 @@ export function Editmode({ children, projectId, defaultChunks }) {
 
     let params = new URL(document.location.href).searchParams;
     setbranch(params.get("em_branch_id"));
+
+    api
+      .get(`/projects/${projectId}`)
+      .then((res) => {
+        const project = res.data
+        if (project.has_watermark) {
+          setHasWaterMark(true)
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }, []);
 
   return (
     <EditmodeContext.Provider value={{ branch, projectId, defaultChunks }}>
       {children}
+      {
+        hasWaterMark && <Watermark />
+      }
     </EditmodeContext.Provider>
   );
 }
