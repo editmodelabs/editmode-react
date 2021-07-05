5393b919a589bb4035c68200bf6d8e44fe2459f1
Jen <jengkarlong@gmail.com>
2021-07-05 14:13:35 +0800

    Fix useGetChunk

--- src/useGetChunk.js
+++ src/useGetChunk.js
@@ -1,16 +1,14 @@
 
 
 import { storeCache, getCachedData, api } from './utilities'
-import { EditmodeContext } from "./EditmodeContext"
 import {useEffect, useState, useContext} from 'react'
 
 export const useGetChunk = (identifier, field = null) => {
-  const [projectId, setProject] = useState(null);
   let [chunk, setChunk] = useState(undefined);
-  const cacheId = identifier + projectId + field;
 
   useEffect(() => {
-    setProject(window["chunksProjectIdentifier"])
+    const projectId = window["chunksProjectIdentifier"];
+    const cacheId = identifier + projectId + field;
     const cachedChunk = getCachedData(cacheId);
     if (cachedChunk) setChunk(JSON.parse(cachedChunk))
     let url = `chunks/${identifier}?project_id=${projectId}`;
@@ -18,11 +16,13 @@ export const useGetChunk = (identifier, field = null) => {
     api
       .get(url)
       .then((res) => {
+        const error = res.data.error || res.data.message
+        if (error) throw error
         storeCache(cacheId, res.data);
         if (!cachedChunk) setChunk(res.data)
       })
       .catch((error) => console.error(error)); // Set error state
-  }, [cacheId])
+  },[])
 
   if (field && chunk && chunk.chunk_type == "collection_item") {
     field = field.toLowerCase()
