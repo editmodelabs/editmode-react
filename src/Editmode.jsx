30755a944767167161723428c283262c64266c6c
Francis Bulus <notchera@gmail.com>
2021-07-08 12:42:26 +0100

    implement branch id props

--- src/Editmode.jsx
+++ src/Editmode.jsx
@@ -4,20 +4,32 @@ import { EditmodeContext } from "./EditmodeContext";
 import { getTimedCachedData, storeTimedCache } from "./utilities";
 import { api } from "./utilities";
 import Watermark from "./Watermark.jsx";
+import { useHotkeys } from "react-hotkeys-hook";
 
-export function Editmode({
-  children,
-  projectId,
-  defaultChunks,
-  branchId = "",
-}) {
-  const [branch, setbranch] = useState(null);
+export function Editmode({ children, projectId, defaultChunks, branchId }) {
+  const [branch, setbranch] = useState("");
   const [hasWaterMark, setHasWaterMark] = useState(null);
+  const [isEditorActive, setIsEditorActive] = useState(false);
+
+  useHotkeys("cmd+shift+e", () => {
+    setIsEditorActive(!isEditorActive);
+  });
+  console.log(isEditorActive);
   if (!projectId) {
     throw new Error("<Editmode projectId={...}> is missing a valid projectId");
   }
   const cacheId = projectId + "_provider";
   useEffect(() => {
+    let params = new URL(document.location.href).searchParams;
+    let queryParams = new URLSearchParams(window.location.search);
+    branchId ? setbranch(branchId) : setbranch(params.get("em_branch_id"));
+    setIsEditorActive(window.location.href.indexOf("editmode") > -1);
+
+    if (branchId && isEditorActive) {
+      queryParams.set("em_branch_id", `${branchId}`);
+      history.pushState(null, null, "?" + queryParams.toString());
+    }
+
     window["chunksProjectIdentifier"] = projectId;
     window["chunksAppFramework"] = "reactjs";
     const cachedItem = getTimedCachedData(cacheId);
@@ -27,12 +39,6 @@ export function Editmode({
     script.src =
       "https://unpkg.com/editmode-magic-editor@^0/dist/magic-editor.js";
     document.body.append(script);
-    let params = new URL(document.location.href).searchParams;
-    if (branchId) {
-      setbranch(branchId);
-    } else {
-      setbranch(params.get("em_branch_id"));
-    }
 
     if (!cachedItem) {
       api
@@ -51,7 +57,7 @@ export function Editmode({
       const project = cachedItem;
       if (project.has_watermark) setHasWaterMark(true);
     }
-  }, []);
+  }, [branch, isEditorActive]);
 
   return (
     <EditmodeContext.Provider value={{ branch, projectId, defaultChunks }}>
