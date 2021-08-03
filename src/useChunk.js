520e6d7035f76faa489bb350dd1265789799ee3f
Francis Bulus <notchera@gmail.com>
2021-08-03 08:33:47 +0100

    refactor usechunk to have next option

--- src/useChunk.js
+++ src/useChunk.js
@@ -14,7 +14,8 @@ export function useChunk(
   defaultContent,
   { identifier, type, contentKey, field }
 ) {
-  const { projectId, defaultChunks, branch } = useContext(EditmodeContext);
+  const { projectId, defaultChunks, branch, next } =
+    useContext(EditmodeContext);
   let [chunk, setChunk] = useState(undefined);
 
   if (!contentKey) {
@@ -41,38 +42,57 @@ export function useChunk(
   }
 
   useEffect(() => {
-    let params = new URL(document.location.href).searchParams;
-    const branchId = branch || params.get("em_branch_id") || "";
-    const branchParams = (branchId && `branch_id=${branchId}`) || "";
-    let url = `chunks/${
-      identifier || contentKey
-    }?project_id=${projectId}&${branchParams}`;
-    if (branchId) cacheId += branchId;
-    let cachedChunk = getCachedData(cacheId);
-    let newChunk = cachedChunk
-      ? JSON.parse(cachedChunk)
-      : fallbackChunk || {
-          chunk_type: type || "single_line_text",
-          content: defaultContent,
-          content_key: contentKey,
-        };
+    if (!next) {
+      let params = new URL(document.location.href).searchParams;
+      const branchId = branch || params.get("em_branch_id") || "";
+      const branchParams = (branchId && `branch_id=${branchId}`) || "";
+      let url = `chunks/${
+        identifier || contentKey
+      }?project_id=${projectId}&${branchParams}`;
+      if (branchId) cacheId += branchId;
+      let cachedChunk = getCachedData(cacheId);
+      let newChunk = cachedChunk
+        ? JSON.parse(cachedChunk)
+        : fallbackChunk || {
+            chunk_type: type || "single_line_text",
+            content: defaultContent,
+            content_key: contentKey,
+          };
 
-    if (newChunk) setChunk(newChunk);
+      if (newChunk) setChunk(newChunk);
 
-    // Fetch new data
-    let error;
-    api
-      .get(url)
-      .then((res) => {
-        storeCache(cacheId, res.data);
-        if (!cachedChunk) setChunk(res.data);
-      }) // Store chunk to localstorage
-      .catch((error) => console.log(error)); // Set error state
+      // Fetch new data
+      let error;
+      api
+        .get(url)
+        .then((res) => {
+          storeCache(cacheId, res.data);
+          if (!cachedChunk) setChunk(res.data);
+        }) // Store chunk to localstorage
+        .catch((error) => console.log(error)); // Set error state
 
-    if (error && identifier) {
-      console.warn(
-        `Something went wrong trying to retrieve chunk data: ${error}. Have you provided the correct Editmode identifier (${identifier}) as a prop to your Chunk component instance?`
-      );
+      if (error && identifier) {
+        console.warn(
+          `Something went wrong trying to retrieve chunk data: ${error}. Have you provided the correct Editmode identifier (${identifier}) as a prop to your Chunk component instance?`
+        );
+      }
+    } else if (next && defaultChunks) {
+      let fallbackChunk;
+      if (identifier) {
+        fallbackChunk = defaultChunks.find((chunkItem) => {
+          return (
+            chunkItem.identifier === identifier ||
+            chunkItem.content_key === identifier
+          );
+        });
+      } else {
+        fallbackChunk = defaultChunks.find(
+          (chunkItem) =>
+            chunkItem.content_key === contentKey &&
+            chunkItem.project_id == projectId
+        );
+      }
+      setChunk(fallbackChunk);
     }
   }, [cacheId, branch]);
 
