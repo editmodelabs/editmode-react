7c4fc897090f82a9491539668ab8412477486fea
Francis Bulus <notchera@gmail.com>
2022-01-17 06:10:20 +0100

    add core convepts section to onboarding

---
+++ src/onboarding/steps/coreConcepts/Steps/StepOne.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+const StepZero = () => {
+  return (
+    <div class="bg-white">
+      <div class="max-w-7xl mx-auto py-16 px-4 sm:px-6 lg:px-8">
+        <div class="bg-indigo-700 rounded-lg shadow-xl overflow-hidden lg:grid lg:grid-cols-2 lg:gap-4">
+          <div class="pt-10 pb-12 px-6 sm:pt-16 sm:px-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
+            <div class="lg:self-center">
+              <h2 class="text-2xl font-extrabold text-white sm:text-4xl">
+                <span class="block"> Welcome </span>
+              </h2>
+              <p class="mt-4 text-lg leading-6 text-indigo-200">
+                In this section, you'll learn a little a bit about what Editmode
+                does.
+              </p>
+            </div>
+          </div>
+          <div class="-mt-6 aspect-w-5 aspect-h-3 md:aspect-w-2 md:aspect-h-1">
+            <img
+              class="transform translate-x-6 translate-y-6 rounded-md object-cover object-left-top sm:translate-x-16 lg:translate-y-20"
+              src="https://img.editmode.com/production/content_pieces/cnk_94a926ac8209abd94b2c/98CipzYMT2eD/images/original/Screenshot_2020-06-01_at_13.41.56.png"
+              alt="App screenshot"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default StepZero;
---
+++ src/onboarding/steps/coreConcepts/Steps/StepTwo.js
@@ -0,0 +1,156 @@
+export default function StepHalf() {
+  return (
+    <div className="relative py-16 bg-white overflow-hidden">
+      <div className="hidden lg:block lg:absolute lg:inset-y-0 lg:h-full lg:w-full">
+        <div
+          className="relative flex justify-end h-full text-lg max-w-prose mx-auto"
+          aria-hidden="true"
+        >
+          <svg
+            className="transform translate-x-32"
+            width={180}
+            height={180}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="74b3fd99-0a6f-4271-bef2-e80eeafdf357"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#74b3fd99-0a6f-4271-bef2-e80eeafdf357)"
+            />
+          </svg>
+          <svg
+            className="absolute top-1/2 right-full transform -translate-y-1/2 -translate-x-32"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="f210dbf6-a58d-4871-961e-36d5016a0f49"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#f210dbf6-a58d-4871-961e-36d5016a0f49)"
+            />
+          </svg>
+          <svg
+            className="absolute bottom-12 left-full transform translate-x-32"
+            width={404}
+            height={384}
+            fill="none"
+            viewBox="0 0 404 384"
+          >
+            <defs>
+              <pattern
+                id="d3eb07ae-5182-43e6-857d-35c643af9034"
+                x={0}
+                y={0}
+                width={20}
+                height={20}
+                patternUnits="userSpaceOnUse"
+              >
+                <rect
+                  x={0}
+                  y={0}
+                  width={4}
+                  height={4}
+                  className="text-gray-200"
+                  fill="currentColor"
+                />
+              </pattern>
+            </defs>
+            <rect
+              width={404}
+              height={384}
+              fill="url(#d3eb07ae-5182-43e6-857d-35c643af9034)"
+            />
+          </svg>
+        </div>
+      </div>
+      <div className="relative px-4 sm:px-6 lg:px-8">
+        <div className="text-lg max-w-prose mx-auto">
+          <h1>
+            <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+              Welcome To
+            </span>
+            <span className="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+              Editmode
+            </span>
+          </h1>
+        </div>
+        <div className="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          <p className="mb-5">
+            Editmode is one place in which you - or your team - can create,
+            manage and update content for your website or web app in a safe,
+            secure way.
+          </p>
+          <p>
+            It's designed to eliminate code alterations when making changes to
+            content of all kinds.
+          </p>
+          <div className="text-lg max-w-prose mx-auto mt-5">
+            <h1>
+              <span className="block text-base text-center text-indigo-600 font-semibold tracking-wide uppercase">
+                A FEW THINGS YOU CAN DO WITH EDITMODE
+              </span>
+            </h1>
+          </div>
+
+          <ul
+            role="list"
+            className="mt-4 flex flex-col justify-between list-disc"
+          >
+            <li className="mb-3">
+              {/* <span className="font-bold">Step 1</span>: Hit the keyboard
+              shortcut - CMD/CTRL + Shift + E - to activate the editor{" "} */}
+              Make content changes to websites and web apps without involving a
+              developer
+            </li>
+            <li className="mb-3">Modify content inline on your website</li>
+            <li>
+              Collaborate on content changes and share internally before
+              publishing
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
